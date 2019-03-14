8449fd4a552b39e08676b4fcb9019d0dd0db70f9
mlambers <31807394+mlambers@users.noreply.github.com>
2019-03-14 18:47:53 +0700

    #4 Commit [Version 0.3b]
    [+] Add support so user can adjust x position manually.

--- ManaBar.lua
+++ ManaBar.lua
@@ -1,5 +1,5 @@
 --------------------------------
---- ManaBar.lua Version 0.3a ---
+--- ManaBar.lua Version 0.3b ---
 --------------------------------
 
 local ManaBar = {
@@ -7,13 +7,14 @@ local ManaBar = {
 	OffsetWidth = Menu.AddOption({"mlambers", "ManaBar"}, "2. Width", "", 20, 200, 2),
 	OffsetHeight = Menu.AddOption({"mlambers", "ManaBar"}, "3. Height", "", 5, 20, 1),
 	OffsetYPos = Menu.AddOption({"mlambers", "ManaBar"}, "4. Y position", "", -20, 20, 1),
+	OffsetXPos = Menu.AddOption({"mlambers", "ManaBar"}, "5. X position", "", -90, 90, 1),
 	NeedInit = true
 }
 
 local MyHero = nil
 local gObject, gObjectOrigin = nil, nil
 local w2sX, w2sY = nil, nil
-local oWidth, oHeight, oYPos = nil, nil, nil
+local oWidth, oHeight, oYPos, oXPos = nil, nil, nil, nil
 local xBox, yBox = nil, nil
 local TopMost, RightMost = nil, nil
 local mCeil = math.ceil
@@ -29,10 +30,12 @@ function ManaBar.OnMenuOptionChange(option, old, new)
 		option == ManaBar.OffsetWidth
 		or option == ManaBar.OffsetHeight
 		or option == ManaBar.OffsetYPos
+		or option == ManaBar.OffsetXPos
 	then
 		oWidth = Menu.GetValue(ManaBar.OffsetWidth)
 		oHeight = Menu.GetValue(ManaBar.OffsetHeight)
 		oYPos = Menu.GetValue(ManaBar.OffsetYPos)
+		oXPos = Menu.GetValue(ManaBar.OffsetXPos)
     end
 end
 
@@ -40,24 +43,24 @@ function ManaBar.OnScriptLoad()
 	MyHero = nil
 	gObject, gObjectOrigin = nil, nil
 	w2sX, w2sY = nil, nil
-	oWidth, oHeight, oYPos = nil, nil, nil
+	oWidth, oHeight, oYPos, oXPos = nil, nil, nil, nil
 	xBox, yBox = nil, nil
 	TopMost, RightMost = nil, nil
 	ManaBar.NeedInit = true
 	
-	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.3a ] Script load.")
+	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.3b ] Script load.")
 end
 
 function ManaBar.OnGameEnd()
 	MyHero = nil
 	gObject, gObjectOrigin = nil, nil
 	w2sX, w2sY = nil, nil
-	oWidth, oHeight, oYPos = nil, nil, nil
+	oWidth, oHeight, oYPos, oXPos = nil, nil, nil, nil
 	xBox, yBox = nil, nil
 	TopMost, RightMost = nil, nil
 	ManaBar.NeedInit = true
 	
-	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.3a ] Game end. Reset all variable.")
+	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.3b ] Game end. Reset all variable.")
 end
 
 function ManaBar.IsOnScreen(tempX, tempY)
@@ -83,11 +86,12 @@ function ManaBar.OnDraw()
 		oWidth = Menu.GetValue(ManaBar.OffsetWidth)
 		oHeight = Menu.GetValue(ManaBar.OffsetHeight)
 		oYPos = Menu.GetValue(ManaBar.OffsetYPos)
+		oXPos = Menu.GetValue(ManaBar.OffsetXPos)
 		xBox, yBox = nil, nil
 		RightMost, TopMost = Renderer.GetScreenSize()
 		ManaBar.NeedInit = false
 		
-		Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.3a ] Game started, init script done.")
+		Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.3b ] Game started, init script done.")
 	end
 	
 	if MyHero == nil then return end
@@ -114,8 +118,8 @@ function ManaBar.OnDraw()
 				gObjectOrigin:SetZ(gObjectOrigin:GetZ() + NPC.GetHealthBarOffset(gObject))
 				w2sX, w2sY = Renderer.WorldToScreen(gObjectOrigin)
 				
-				xBox = w2sX - mCeil(oWidth * 0.5)
-				yBox = w2sY - mCeil(oHeight * 0.5) - oYPos
+				xBox = w2sX + oXPos
+				yBox = w2sY + oYPos
 				
 				--[[
 					Draw black background.
