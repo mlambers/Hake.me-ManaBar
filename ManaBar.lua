755a48926ac92e5fe44a0411dbc0ab8db62a5dd5
mlambers <mlambers@mail.ccsf.edu>
2019-09-27 21:42:18 +0700

    #7 Commit [ Version 0.4a ]
    [*] Reformat variable names.

--- ManaBar.lua
+++ ManaBar.lua
@@ -1,67 +1,85 @@
--------------------------------
---- ManaBar.lua Version 0.4 ---
--------------------------------
+--------------------------------
+--- ManaBar.lua Version 0.4a ---
+--------------------------------
 
 local ManaBar = {
 	OptionEnable = Menu.AddOption({"mlambers", "ManaBar"}, "1. Enable.", "Enable/Disable this script."),
-	OffsetWidth = Menu.AddOption({"mlambers", "ManaBar"}, "2. Width", "", 20, 200, 2),
-	OffsetHeight = Menu.AddOption({"mlambers", "ManaBar"}, "3. Height", "", 5, 20, 1),
-	OffsetYPos = Menu.AddOption({"mlambers", "ManaBar"}, "4. Y position", "", -20, 20, 1),
-	OffsetXPos = Menu.AddOption({"mlambers", "ManaBar"}, "5. X position", "", -90, 90, 1)
+	OffsetWidth = Menu.AddOption({"mlambers", "ManaBar"}, "2. Width", "", 20, 220, 2),
+	OffsetHeight = Menu.AddOption({"mlambers", "ManaBar"}, "3. Height", "", 2, 30, 1),
+	OffsetYPos = Menu.AddOption({"mlambers", "ManaBar"}, "4. Y position", "", -50, 50, 1),
+	OffsetXPos = Menu.AddOption({"mlambers", "ManaBar"}, "5. X position", "", -120, 120, 1)
 }
 
+--[[
+    Localize global function from _ENV
+        math.ceil
+--]]
+local MathCeil = math.ceil
+
 local MyHero = nil
-local gObject, gObjectOrigin = nil, nil
-local w2sX, w2sY = nil, nil
-local oWidth, oHeight, oYPos, oXPos = nil, nil, nil, nil
-local xBox, yBox = nil, nil
-local TopMost, RightMost = nil, nil
-local mCeil = math.ceil
+local hero_object, hero_origin = nil, nil
+
+local bar_width, bar_height = nil, nil
+local bar_x_offset, bar_y_offset = nil, nil
+local bar_x, bar_y = nil, nil
+
+local x_w2s, y_w2s = nil, nil
+local screen_width, screen_height = nil, nil
+
 
 function ManaBar.OnMenuOptionChange(option, old, new)
-	if Engine.IsInGame() == false then return end
-	if Menu.IsEnabled(ManaBar.OptionEnable) == false then return end
 	if MyHero == nil then return end
-	
-	if not option then return end
-	
+    
+    if option == ManaBar.OptionEnable and old == 0 then
+        bar_width = Menu.GetValue(ManaBar.OffsetWidth)
+		bar_height = Menu.GetValue(ManaBar.OffsetHeight)
+        bar_x_offset = Menu.GetValue(ManaBar.OffsetXPos)
+		bar_y_offset = Menu.GetValue(ManaBar.OffsetYPos)
+    end
+    
     if 
-		option == ManaBar.OffsetWidth
+		(option == ManaBar.OffsetWidth
 		or option == ManaBar.OffsetHeight
 		or option == ManaBar.OffsetYPos
-		or option == ManaBar.OffsetXPos
+		or option == ManaBar.OffsetXPos)
 	then
-		oWidth = Menu.GetValue(ManaBar.OffsetWidth)
-		oHeight = Menu.GetValue(ManaBar.OffsetHeight)
-		oYPos = Menu.GetValue(ManaBar.OffsetYPos)
-		oXPos = Menu.GetValue(ManaBar.OffsetXPos)
+		bar_width = Menu.GetValue(ManaBar.OffsetWidth)
+		bar_height = Menu.GetValue(ManaBar.OffsetHeight)
+        bar_x_offset = Menu.GetValue(ManaBar.OffsetXPos)
+		bar_y_offset = Menu.GetValue(ManaBar.OffsetYPos)
     end
 end
 
 function ManaBar.OnScriptLoad()
 	MyHero = nil
-	gObject, gObjectOrigin = nil, nil
-	w2sX, w2sY = nil, nil
-	oWidth, oHeight, oYPos, oXPos = nil, nil, nil, nil
-	xBox, yBox = nil, nil
-	TopMost, RightMost = nil, nil
+	hero_object, hero_origin = nil, nil
 	
-	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.4 ] [ GameState : " .. GameRules.GetGameState() .. " ] Script load.")
+    bar_width, bar_height = nil, nil
+    bar_x_offset, bar_y_offset = nil, nil
+	bar_x, bar_y = nil, nil
+    
+    x_w2s, y_w2s = nil, nil
+	screen_width, screen_height = nil, nil
+	
+	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.4a ] Script load.")
 end
 
 function ManaBar.OnGameEnd()
 	MyHero = nil
-	gObject, gObjectOrigin = nil, nil
-	w2sX, w2sY = nil, nil
-	oWidth, oHeight, oYPos, oXPos = nil, nil, nil, nil
-	xBox, yBox = nil, nil
-	TopMost, RightMost = nil, nil
+	hero_object, hero_origin = nil, nil
 	
-	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.4 ] [ GameState : " .. GameRules.GetGameState() .. " ] Game end. Reset all variable.")
+	bar_width, bar_height = nil, nil
+    bar_x_offset, bar_y_offset = nil, nil
+	bar_x, bar_y = nil, nil
+    
+    x_w2s, y_w2s = nil, nil
+	screen_width, screen_height = nil, nil
+	
+	Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.4a ] Game end. Reset all variable.")
 end
 
-function ManaBar.IsOnScreen(tempX, tempY)
-	if (tempX < 1) or (tempY < 1) or (tempX > RightMost) or (tempY > TopMost) then 
+function ManaBar.IsOnScreen(x_position, y_position)
+	if (x_position < 1) or (y_position < 1) or (x_position > screen_width) or (y_position > screen_height) then 
 		return false 
 	end
 	
@@ -71,18 +89,20 @@ end
 function ManaBar.OnUpdate()
 	if Menu.IsEnabled(ManaBar.OptionEnable) == false then return end
 	
-	if MyHero == nil or MyHero ~= Heroes.GetLocal() then 
-		MyHero = Heroes.GetLocal()
-		gObject, gObjectOrigin = nil, nil
-		w2sX, w2sY = nil, nil
-		oWidth = Menu.GetValue(ManaBar.OffsetWidth)
-		oHeight = Menu.GetValue(ManaBar.OffsetHeight)
-		oYPos = Menu.GetValue(ManaBar.OffsetYPos)
-		oXPos = Menu.GetValue(ManaBar.OffsetXPos)
-		xBox, yBox = nil, nil
-		RightMost, TopMost = Renderer.GetScreenSize()
+	if MyHero == nil or MyHero ~= Heroes.GetLocal() then
+		bar_width = Menu.GetValue(ManaBar.OffsetWidth)
+		bar_height = Menu.GetValue(ManaBar.OffsetHeight)
+        bar_x_offset = Menu.GetValue(ManaBar.OffsetXPos)
+		bar_y_offset = Menu.GetValue(ManaBar.OffsetYPos)
+		bar_x, bar_y = nil, nil
+        
+        x_w2s, y_w2s = nil, nil
+		screen_width, screen_height = Renderer.GetScreenSize()
+        
+        hero_object, hero_origin = nil, nil
+        MyHero = Heroes.GetLocal()
 		
-		Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.4 ] [ GameState : " .. GameRules.GetGameState() .. " ] Game started, init script done.")
+		Console.Print("[" .. os.date("%I:%M:%S %p") .. "] - - [ ManaBar.lua ] [ Version 0.4a ] Game started, init script done.")
 		return
 	end
 end
@@ -90,52 +110,44 @@ end
 function ManaBar.OnDraw()
 	if Menu.IsEnabled(ManaBar.OptionEnable) == false then return end
 	
-	if
-		Engine.IsInGame() == false
-		or GameRules.GetGameState() < 4 
-		or GameRules.GetGameState() > 5
-	then
-		return
-	end
-	
 	if MyHero == nil then return end
 
 	for i = 1, Heroes.Count() do
-		gObject = Heroes.Get(i)
+		hero_object = Heroes.Get(i)
 		
 		if 
-			gObject ~= nil
-			and Entity.IsDormant(gObject) == false
-			and Entity.IsAlive(gObject)
-			and Entity.IsSameTeam(MyHero, gObject) == false
-			and Entity.GetField(gObject, "m_bIsIllusion") == false
-			and NPC.IsIllusion(gObject) == false
-			and Entity.IsPlayer(Entity.GetOwner(gObject)) 
+			hero_object ~= nil
+			and Entity.IsDormant(hero_object) == false
+			and Entity.IsAlive(hero_object)
+			and Entity.IsSameTeam(MyHero, hero_object) == false
+			and Entity.GetField(hero_object, "m_bIsIllusion") == false
+			and NPC.IsIllusion(hero_object) == false
+			and Entity.IsPlayer(Entity.GetOwner(hero_object)) 
 		then
-			gObjectOrigin = Entity.GetAbsOrigin(gObject)
-			w2sX, w2sY = Renderer.WorldToScreen(gObjectOrigin)
+			hero_origin = Entity.GetAbsOrigin(hero_object)
+			x_w2s, y_w2s = Renderer.WorldToScreen(hero_origin)
 			
 			--[[
 				Need to check if target object on our screen or not.
 			--]]
-			if ManaBar.IsOnScreen(w2sX, w2sY) then
-				gObjectOrigin:SetZ(gObjectOrigin:GetZ() + NPC.GetHealthBarOffset(gObject))
-				w2sX, w2sY = Renderer.WorldToScreen(gObjectOrigin)
+			if ManaBar.IsOnScreen(x_w2s, y_w2s) then
+				hero_origin:SetZ(hero_origin:GetZ() + NPC.GetHealthBarOffset(hero_object))
+				x_w2s, y_w2s = Renderer.WorldToScreen(hero_origin)
 				
-				xBox = w2sX + oXPos
-				yBox = w2sY + oYPos
+				bar_x = x_w2s + bar_x_offset
+				bar_y = y_w2s + bar_y_offset
 				
 				--[[
 					Draw black background.
 				--]]
 				Renderer.SetDrawColor(0, 0, 0, 255)
-				Renderer.DrawFilledRect(xBox, yBox, oWidth, oHeight)
+				Renderer.DrawFilledRect(bar_x, bar_y, bar_width, bar_height)
 				
 				--[[
 					Draw the actual mana bar.
 				--]]
 				Renderer.SetDrawColor(79, 120, 249, 255)
-				Renderer.DrawFilledRect((1 + xBox), (1 + yBox), mCeil((oWidth - 2) * (NPC.GetMana(gObject) /  NPC.GetMaxMana(gObject))), (oHeight - 2))
+				Renderer.DrawFilledRect((1 + bar_x), (1 + bar_y), MathCeil((bar_width - 2) * (NPC.GetMana(hero_object) /  NPC.GetMaxMana(hero_object))), (bar_height - 2))
 			end
 		end
 	end
